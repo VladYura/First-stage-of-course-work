510b05fefb65b1addef4f4dda71bd435038b03ab
VladYura <vlad.yurashevich02@mail.ru>
2022-11-02 16:54:07 +0300

    Second commit(V2)

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import copy
+
+
 # Функция для первого сокращения (она отличается от mid_game, так как входной first_list имеет другую размерность)
 def start_progress(first_list):
     output = []
@@ -9,7 +12,7 @@ def start_progress(first_list):
         if len(first_list) == len_gr:
             break
 
-        last_no_repeat_item = no_list_append(first_list, group_list, 0, 0)
+        last_no_repeat_item = no_list_append(first_list, group_list, 0)
         group_list = [last_no_repeat_item[0]]
         group = f'B{iterate}'
 
@@ -49,13 +52,13 @@ def conclusion_dict(group, output):
 # Возвращает состояние, которое не повторялось в промежуточном списке (это который формирует output)
 # Принимает ключ key, так как первое сокращение отличается от последующих
 # Index - это значение последнего не повторяющегося состояния (иначе output будет состоять из одинаковых групп)
-def no_list_append(list1, list2, key, index):
+def no_list_append(list1, list2, key):
     if key == 0:
         for j in range(len(list1)):
             if list1[j] not in list2:
                 return [list1[j], j]
     else:
-        for j in range(index, len(list1[1])):
+        for j in range(len(list1[1])):
             if list1[1][j] not in list2:
                 return [list1[1][j], j]
     return list1[1]
@@ -90,35 +93,42 @@ def super_last_func(output, dic_out):
 # Основная функция дял сокращения
 # Принимает предыдущий output и букву группы
 def mid_game(first_list, group_name):
+    copy_first_list = copy.deepcopy(first_list)
     output = []
-    len_gr = 0
-    group_list = []
     iterate = 1
-    last_no_repeat_item = [0, 0]
+    remove_list = []
 
     while True:
         # Во всех сокращениях кроме последнего мы сокращаем только первую группу
         # Поэтому здесь прописан if, запускающий функцию для последнего сокращения
         if len(first_list[0][1]) == 1:
             return func_last_stage(first_list, group_name)
-        else:
-            last_no_repeat_item = no_list_append(first_list[0], group_list, 1, last_no_repeat_item[1])
 
         group = f'{group_name}{iterate}'
-        group_list = [last_no_repeat_item[0]]
-
-        # If, позволяющий выходить из while
-        if not find_the_same_items(output, group_list):
+        try:
+            group_list = [copy_first_list[0][1][0]]
+        except IndexError:
             break
+        else:
+            # If, позволяющий выходить из while
+            if not find_the_same_items(output, group_list):
+                break
 
-        # Поиск повторяющихся групп и формирование промежуточного списка
-        for j in range(last_no_repeat_item[1] + 1, len(first_list[0][1])):
-            if first_list[0][1][j][1] == group_list[0][1] and find_the_same_items(output, group_list):
-                group_list.append(first_list[0][1][j])
+            # Поиск повторяющихся групп и формирование промежуточного списка
+            for j in copy_first_list[0][1]:
+                if first_list[0][1].index(j) == 0:
+                    remove_list.append(j)
+                    continue
+                if j[1] == group_list[0][1]:
+                    remove_list.append(j)
+                    group_list.append(j)
 
-        iterate += 1
-        len_gr += len(group_list)
-        output.append([group, group_list])
+            for item in remove_list:
+                copy_first_list[0][1].remove(item)
+            remove_list.clear()
+
+            iterate += 1
+            output.append([group, group_list])
 
     # Цикл для занесения оставшихся групп в output
     for i in range(1, len(first_list)):
@@ -135,7 +145,6 @@ def mid_game(first_list, group_name):
 # Принцип тот же, что и у mid_game, только для второй группы
 def func_last_stage(first_list, group_name):
     group_list = []
-    a = [0, 0]
     iterate = 1
     len_gr = 0
 
@@ -147,7 +156,7 @@ def func_last_stage(first_list, group_name):
     while True:
         if len(first_list[1][1]) == len_gr:
             break
-        a = no_list_append(first_list[1], group_list, 1, a[1])
+        a = no_list_append(first_list[1], group_list, 1)
         group_list = [a[0]]
         group = f'{group_name}{iterate}'
 
@@ -166,3 +175,17 @@ def func_last_stage(first_list, group_name):
         iterate += 1
 
     return output
+
+
+def count_groups(output, first_list):
+    count = 0
+    for group in output:
+        count += len(group[1])
+    print(count, len(first_list[0][1]))
+    if count == len(first_list[0][1]):
+        return True
+    return False
+
+
+
+
